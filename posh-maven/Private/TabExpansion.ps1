4324ae2678da60126395449109de409d3f8b3c4e
Clijsters <dominique.clijsters@gmail.com>
2020-02-18 20:55:40 +0100

    Move more goals to new map format

--- posh-maven/Private/TabExpansion.ps1
+++ posh-maven/Private/TabExpansion.ps1
@@ -1,5 +1,22 @@
-
 [String[]]$goals = @(
+    "-Dmaven.test.skip=true",
+    "-DskipTests",
+    "-DskipITs",
+    "-Dtest",
+    "-Dit.test",
+    "-DfailIfNoTests",
+    "-Dmaven.surefire.debug",
+    "-DenableCiProfile",
+    "-Dpmd.skip=true",
+    "-Dcheckstyle.skip=true",
+    "-Dtycho.mode=maven",
+    "-Dmaven.javadoc.skip=true",
+    "-Dgwt.compiler.skip",
+    "-Dcobertura.skip=true",
+    "-Dfindbugs.skip=true",
+    "-DperformRelease=true",
+    "-Dgpg.skip=true",
+    "-DforkCount"
     "validate",
     "initialize",
     "generate-sources",
@@ -34,54 +51,13 @@
 ) | Sort-Object
 
 $completeMap = @{
-    appengine = @(
-        "backends_configure",
-        "backends_delete",
-        "backends_rollback",
-        "backends_start",
-        "backends_stop",
-        "backends_update",
-        "debug",
-        "devserver",
-        "devserver_start",
-        "devserver_stop",
-        "endpoints_get_client_lib",
-        "endpoints_get_discovery_doc",
-        "enhance",
-        "rollback",
-        "set_default_version",
-        "start_module_version",
-        "stop_module_version",
-        "update",
-        "update_cron",
-        "update_dos",
-        "update_indexes",
-        "update_queues",
-        "vacuum_indexes"
-    );
-    dependency = @(
-        "analyze",
-        "analyze-dep-mgt",
-        "analyze-duplicate",
-        "analyze-only",
-        "analyze-report",
-        "build-classpath",
-        "copy",
-        "copy-dependencies",
-        "get",
-        "go-offline",
-        "help",
-        "list",
-        "list-repositories",
-        "properties",
-        "purge-local-repository",
-        "resolve",
-        "resolve-plugins",
-        "sources",
-        "tree",
-        "unpack",
-        "unpack-dependencies"
-    )
+    android        = @("apk", "apklib", "clean", "deploy", "deploy-dependencies", "dex", "emulator-start", "emulator-stop", "emulator-stop-all", "generate-sources", "help", "instrument", "manifest-update", "pull", "push", "redeploy", "run", "undeploy", "unpack", "version-update", "zipalign", "devices");
+    appengine      = @("backends_configure", "backends_delete", "backends_rollback", "backends_start", "backends_stop", "backends_update", "debug", "devserver", "devserver_start", "devserver_stop", "endpoints_get_client_lib", "endpoints_get_discovery_doc", "enhance", "rollback", "set_default_version", "start_module_version", "stop_module_version", "update", "update_cron", "update_dos", "update_indexes", "update_queues", "vacuum_indexes");
+    dependency     = @("analyze", "analyze-dep-mgt", "analyze-duplicate", "analyze-only", "analyze-report", "build-classpath", "copy", "copy-dependencies", "get", "go-offline", "help", "list", "list-repositories", "properties", "purge-local-repository", "resolve", "resolve-plugins", "sources", "tree", "unpack", "unpack-dependencies");
+    "build-helper" = @("add-resource", "add-source", "add-test-resource", "add-test-source", "attach-artifact", "bsh-property", "cpu-count", "help", "local-ip", "maven-version", "parse-version", "regex-properties", "regex-property", "released-version", "remove-project-artifact", "reserve-network-port", "timestamp-property");
+    grails         = @("clean", "config-directories", "console", "create-controller", "create-domain-class", "create-integration-test", "create-pom", "create-script", "create-service", "create-tag-lib", "create-unit-test", "exec", "generate-all", "generate-controller", "generate-views", "help", "init", "init-plugin", "install-templates", "list-plugins", "maven-clean", "maven-compile", "maven-functional-test", "maven-grails-app-war", "maven-test", "maven-war", "package", "package-plugin", "run-app", "run-app-https", "run-war", "set-version", "test-app", "upgrade", "validate", "validate-plugin", "war")
+    gwt            = @("browser", "clean", "compile", "compile-report", "css", "debug", "eclipse", "eclipseTest", "generateAsync", "help", "i18n", "mergewebxml", "resources", "run", "run-codeserver", "sdkInstall", "source-jar", "soyc", "test")
+    versions       = @("display-dependency-updates", "display-plugin-updates", "display-property-updates", "update-parent", "update-properties", "update-child-modules", "lock-snapshots", "unlock-snapshots", "resolve-ranges", "set", "use-releases", "use-next-releases", "use-latest-releases", "use-next-snapshots", "use-latest-snapshots", "use-next-versions", "use-latest-versions", "commit", "revert")
 } | Sort-Object
 
 if (Test-Path Function:\TabExpansion) {
