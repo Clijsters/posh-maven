2c7fbbfa60f611e37128705daa5e8f9f1c934d6d
Clijsters <dominique.clijsters@gmail.com>
2020-02-18 18:18:58 +0100

    Add goals as hashmap. tbd

--- posh-maven/Private/TabExpansion.ps1
+++ posh-maven/Private/TabExpansion.ps1
@@ -33,12 +33,56 @@
     "release"
 ) | Sort-Object
 
-[String[]]$firstLevelProperties = @(
-    "project",
-    "settings",
-    "env",
-    "project"
-) | Sort-Object
+$completeMap = @{
+    appengine = @(
+        "backends_configure",
+        "backends_delete",
+        "backends_rollback",
+        "backends_start",
+        "backends_stop",
+        "backends_update",
+        "debug",
+        "devserver",
+        "devserver_start",
+        "devserver_stop",
+        "endpoints_get_client_lib",
+        "endpoints_get_discovery_doc",
+        "enhance",
+        "rollback",
+        "set_default_version",
+        "start_module_version",
+        "stop_module_version",
+        "update",
+        "update_cron",
+        "update_dos",
+        "update_indexes",
+        "update_queues",
+        "vacuum_indexes"
+    );
+    dependency = @(
+        "analyze",
+        "analyze-dep-mgt",
+        "analyze-duplicate",
+        "analyze-only",
+        "analyze-report",
+        "build-classpath",
+        "copy",
+        "copy-dependencies",
+        "get",
+        "go-offline",
+        "help",
+        "list",
+        "list-repositories",
+        "properties",
+        "purge-local-repository",
+        "resolve",
+        "resolve-plugins",
+        "sources",
+        "tree",
+        "unpack",
+        "unpack-dependencies"
+    )
+} | Sort-Object
 
 if (Test-Path Function:\TabExpansion) {
     Rename-Item Function:\TabExpansion OldTabExpansion
@@ -55,6 +99,13 @@ function TabExpansion($line, $lastWord) {
             if ($lastLvl -eq "") {
                 return
             }
+        }
+        $lvl = ($completeMap.keys -match "^$($lastWord)")
+        if ($lvl.Count -gt 1) {
+            $lvl
+        }
+        else {
+            $completeMap."$lvl" | % { "$lvl`:$_" }
         }
         $goals -match "^$($lastWord)"
     }
